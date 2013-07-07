8d3e52115672d1cdd8c5b145ebe15785ad53b45e
neeee <ne@tewi>
2013-07-07 17:39:29 +0200

    initial commit

---
+++ mpdviz.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/nsf/termbox-go"
+)
+
+var dbuf [][]bool
+
+var (
+	step  = flag.Int("s", 2, "number of samples to average in each column")
+	dim   = flag.Bool("d", false, "don't use bold")
+	color = flag.String("c", "blue", "which color to use")
+)
+
+var colors = map[string]termbox.Attribute{
+	"default": termbox.ColorDefault,
+	"black":   termbox.ColorBlack,
+	"red":     termbox.ColorRed,
+	"green":   termbox.ColorGreen,
+	"yellow":  termbox.ColorYellow,
+	"blue":    termbox.ColorBlue,
+	"magenta": termbox.ColorMagenta,
+	"cyan":    termbox.ColorCyan,
+	"white":   termbox.ColorWhite,
+}
+
+var on termbox.Attribute
+var off = termbox.ColorBlack
+
+func main() {
+	flag.Parse()
+	var ok bool
+	on, ok = colors[*color]
+	if !ok {
+		die("unknown color: %s", *color)
+	}
+	if !*dim {
+		on = on | termbox.AttrBold
+	}
+
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	refresh()
+
+	// input handler
+	go func() {
+		for {
+			ev := termbox.PollEvent()
+			if ev.Ch == 0 && ev.Key == termbox.KeyCtrlC {
+				termbox.Close()
+				os.Exit(0)
+			}
+		}
+	}()
+
+	buf := make(chan int16, 128)
+	go draw(buf)
+
+	file, err := os.Open("/tmp/mpd.fifo")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		var i int16
+		binary.Read(file, binary.LittleEndian, &i)
+		buf <- i
+	}
+}
+
+func flush() {
+	w, h := len(dbuf[0]), len(dbuf)
+	for x := 0; x < h; x++ {
+		for y := 0; y < w; y++ {
+			if y%2 != 0 {
+				continue
+			}
+
+			up, down := dbuf[x][y], dbuf[x][y+1]
+			switch {
+			// not needed because we never print more than one cell (yet)
+			//case up && down:
+			//	termbox.SetCell(x, y/2, ' ', on, on)
+			case up:
+				termbox.SetCell(x, y/2, '▀', on, off)
+			case down:
+				termbox.SetCell(x, y/2, '▄', on, off)
+			}
+		}
+	}
+	termbox.Flush()
+}
+
+func refresh() {
+	w, h := termbox.Size()
+	h *= 2
+	dbuf = make([][]bool, w)
+	for i := 0; i < w; i++ {
+		dbuf[i] = make([]bool, h)
+		for j := 0; j < h; j++ {
+			dbuf[i][j] = false
+		}
+	}
+}
+
+func draw(buf chan int16) {
+	var pos int
+	var v float64
+	fstep := float64(*step)
+
+	for {
+		w, h := len(dbuf), len(dbuf[0])
+		half_h := float64(h / 2)
+		v = 0
+		for i := 0; i < *step; i++ {
+			v += float64(<-buf)
+		}
+		v /= fstep
+		v /= 32768 / half_h
+		v += half_h
+
+		dbuf[pos][int(v)] = true
+
+		pos++
+		if pos >= w {
+			flush()
+			refresh()
+			termbox.Clear(0, 0)
+			pos = 0
+		}
+	}
+}
+
+func die(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, "error: %s\n",
+		fmt.Sprintf(format, args...))
+	os.Exit(1)
+}
