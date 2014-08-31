892cf6dcb65788bd72d6af4231ab2432782dea40
lucy <ne.tetewi@gmail.com>
2014-08-31 12:36:32 +0200

    welp fix completion

--- mpdviz.go
+++ mpdviz.go
@@ -21,7 +21,7 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.package main
+DEALINGS IN THE SOFTWARE.
 */
 
 package main
@@ -39,7 +39,7 @@ import (
 
 var (
 	color = flag.StringP("color", "c", "default", "Color to use")
-	dim   = flag.BoolP("dim", "d", false,
+	dim = flag.BoolP("dim", "d", false,
 		"Turn off bright colors where possible")
 
 	step  = flag.Int("step", 2, "Samples for each step (wave/lines)")
